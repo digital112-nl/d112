32f49c8c3d3910c479f9264c4186f20cef5b886a
Wesley van der Kop <mail@wesleyvanderkop.nl>
2020-10-10 18:41:39 +0200

    ✨ added login page

--- front-end/src/app/core/auth/auth.module.ts
+++ front-end/src/app/core/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { AuthGuard } from './auth.guard';
 import { AuthRoutes } from './auth.routes';
 import { LoginComponent } from './login/login.component';
@@ -7,6 +8,7 @@ import { LoginComponent } from './login/login.component';
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule,
 
     AuthRoutes
   ],
