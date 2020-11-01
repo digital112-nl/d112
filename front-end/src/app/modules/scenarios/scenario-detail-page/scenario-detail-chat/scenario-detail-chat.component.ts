c52aa78e547e507f80812516ca3e894effd3a2c9
Wesley van der Kop <mail@wesleyvanderkop.nl>
2020-11-01 16:05:36 +0100

    ✨ Added chat info with questionnaire

--- front-end/src/app/modules/scenarios/scenario-detail-page/scenario-detail-chat/scenario-detail-chat.component.ts
+++ front-end/src/app/modules/scenarios/scenario-detail-page/scenario-detail-chat/scenario-detail-chat.component.ts
@@ -1,10 +1,5 @@
 import { Component } from '@angular/core';
 
-export interface ChatMessage {
-  message: string;
-  fromAI: boolean;
-}
-
 @Component({
   selector: 'di-scenario-detail-chat',
   templateUrl: './scenario-detail-chat.component.html',
@@ -12,14 +7,81 @@ export interface ChatMessage {
 })
 export class ScenarioDetailChatComponent {
 
-  public chatMessages: ChatMessage[] = [
+  public questionnaire: any[] = [
     {
-      message: 'Test message',
-      fromAI: false
-    },
-    {
-      message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent efficitur libero eget ligula vulputate fringilla.',
-      fromAI: true
+      key: 'scale-1-10',
+      type: 'Scale',
+      question: 'On a scale from 1 to 10 how is the fire progressing?',
+      asked: true,
+      answered: true,
+      outcome: {
+        type: 'ScalePriorityPossibility',
+        key: 'scale-1',
+        forValue: 1,
+        priority: 2
+      },
+      possibilities: [
+        {
+          type: 'ScalePriorityPossibility',
+          key: 'scale-1',
+          forValue: 1,
+          priority: 2
+        },
+        {
+          type: 'ScalePriorityPossibility',
+          key: 'scale-2',
+          forValue: 2,
+          priority: 2
+        },
+        {
+          type: 'ScalePriorityPossibility',
+          key: 'scale-3',
+          forValue: 3,
+          priority: 2
+        },
+        {
+          type: 'ScalePriorityPossibility',
+          key: 'scale-4',
+          forValue: 4,
+          priority: 2
+        },
+        {
+          type: 'ScalePriorityPossibility',
+          key: 'scale-5',
+          forValue: 5,
+          priority: 1
+        },
+        {
+          type: 'ScalePriorityPossibility',
+          key: 'scale-6',
+          forValue: 6,
+          priority: 1
+        },
+        {
+          type: 'ScalePriorityPossibility',
+          key: 'scale-7',
+          forValue: 7,
+          priority: 1
+        },
+        {
+          type: 'ScalePriorityPossibility',
+          key: 'scale-8',
+          forValue: 8,
+          priority: 0
+        },
+        {
+          type: 'ScalePriorityPossibility',
+          key: 'scale-9',
+          forValue: 9,
+          priority: 0
+        },
+        {
+          type: 'ScalePriorityPossibility',
+          key: 'scale-10',
+          forValue: 10,
+          priority: 0
+        }
+      ]
     }
   ];
 }
